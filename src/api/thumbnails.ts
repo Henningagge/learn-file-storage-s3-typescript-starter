94f8f89a9be42b276b3583aefc0e860edf586e16
heagge <henning.agge@gmail.com>
2025-11-29 05:48:08 +0100

    thumbnail now responses write and saves assets

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -5,6 +5,7 @@ import type { ApiConfig } from '../config';
 import type { BunRequest } from 'bun';
 import { BadRequestError, NotFoundError, UserForbiddenError } from './errors';
 import { Buffer } from 'node:buffer';
+import path from 'node:path';
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -15,32 +16,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log('uploading thumbnail for video', videoId, 'by user', userID);
-
-  // TODO: implement the upload here
   const formData = await req.formData();
   let imageData = formData.get('thumbnail');
+  const videoMetadata = getVideo(cfg.db, videoId);
+  const MAX_UPLOAD_SIZE = 10 << 20;
+
   if (!(imageData instanceof File)) {
     throw new BadRequestError('Thumbnail file missing');
   }
-  const MAX_UPLOAD_SIZE = 10 << 20;
+
   if (imageData.size > MAX_UPLOAD_SIZE) {
     throw new BadRequestError('Filesize is to Large for Upload');
   }
+
   const mediaType = imageData.type;
-  const arrayBuffer = await imageData.arrayBuffer();
-  const buffer: Buffer = Buffer.from(arrayBuffer);
-  let bufferString = buffer.toString('base64');
-  const dataUrl = `data:${mediaType};base64,${bufferString}`;
-  const videoMetadata = getVideo(cfg.db, videoId);
+  if (mediaType !== 'image/jpeg' && mediaType !== 'image/png') {
+    throw new BadRequestError(
+      'Wrong file format found. File is not a JPEG or PNG'
+    );
+  }
+
   if (!videoMetadata) {
     throw new NotFoundError('Metadata could no be found');
   }
   if (videoMetadata?.userID != userID) {
     throw new UserForbiddenError('Wrong UserId for accessing videos');
   }
-  console.log(dataUrl);
-  videoMetadata.thumbnailURL = dataUrl;
-  const updatedVideo = await updateVideo(cfg.db, videoMetadata);
-  return respondWithJSON(200, updatedVideo);
+
+  const filePath = path.join(cfg.assetsRoot, `${videoId}.${mediaType}`);
+
+  videoMetadata.thumbnailURL = `http://localhost:${cfg.port}/assets/${videoId}.${mediaType}`;
+  Bun.write(filePath, imageData);
+  updateVideo(cfg.db, videoMetadata);
+
+  return respondWithJSON(200, videoMetadata);
 }
