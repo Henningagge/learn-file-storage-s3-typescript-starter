27d385161037947f888a347d638f1d9d57ec6671
heagge <henning.agge@gmail.com>
2025-12-03 06:14:30 +0100

    signed urls don't work

--- src/api/video-meta.ts
+++ src/api/video-meta.ts
@@ -1,17 +1,17 @@
-import { type ApiConfig } from "../config";
-import { getBearerToken, validateJWT } from "../auth";
-import { createVideo, deleteVideo, getVideo, getVideos } from "../db/videos";
-import { respondWithJSON } from "./json";
-import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import type { BunRequest } from "bun";
-
+import { type ApiConfig } from '../config';
+import { getBearerToken, validateJWT } from '../auth';
+import { createVideo, deleteVideo, getVideo, getVideos } from '../db/videos';
+import { respondWithJSON } from './json';
+import { BadRequestError, NotFoundError, UserForbiddenError } from './errors';
+import type { BunRequest } from 'bun';
+import { dbVideoToSignedVideo } from './videos';
 export async function handlerVideoMetaCreate(cfg: ApiConfig, req: Request) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
   const { title, description } = await req.json();
   if (!title || !description) {
-    throw new BadRequestError("Missing title or description");
+    throw new BadRequestError('Missing title or description');
   }
 
   const video = createVideo(cfg.db, {
@@ -26,18 +26,19 @@ export async function handlerVideoMetaCreate(cfg: ApiConfig, req: Request) {
 export async function handlerVideoMetaDelete(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
   if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
+    throw new BadRequestError('Invalid video ID');
   }
 
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  const video = getVideo(cfg.db, videoId);
+  let video = getVideo(cfg.db, videoId);
+
   if (!video) {
     throw new NotFoundError("Couldn't find video");
   }
   if (video.userID !== userID) {
-    throw new UserForbiddenError("Not authorized to delete this video");
+    throw new UserForbiddenError('Not authorized to delete this video');
   }
 
   deleteVideo(cfg.db, videoId);
@@ -47,15 +48,18 @@ export async function handlerVideoMetaDelete(cfg: ApiConfig, req: BunRequest) {
 export async function handlerVideoGet(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
   if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
+    throw new BadRequestError('Invalid video ID');
   }
 
-  const video = getVideo(cfg.db, videoId);
+  let video = getVideo(cfg.db, videoId);
   if (!video) {
     throw new NotFoundError("Couldn't find video");
   }
-
-  return respondWithJSON(200, video);
+  if (!video.videoURL) {
+    return respondWithJSON(200, video);
+  }
+  let videoSigned = await dbVideoToSignedVideo(cfg, video);
+  return respondWithJSON(200, videoSigned);
 }
 
 export async function handlerVideosRetrieve(cfg: ApiConfig, req: Request) {
@@ -63,5 +67,18 @@ export async function handlerVideosRetrieve(cfg: ApiConfig, req: Request) {
   const userID = validateJWT(token, cfg.jwtSecret);
 
   const videos = getVideos(cfg.db, userID);
-  return respondWithJSON(200, videos);
+  if (!videos[0]) {
+    return respondWithJSON(200, videos);
+  }
+  let signedVideos = [];
+  for (let video of videos) {
+    if (!video.videoURL) {
+      signedVideos.push(video);
+      continue;
+    }
+    let video1 = await dbVideoToSignedVideo(cfg, video);
+    signedVideos.push(video1);
+  }
+
+  return respondWithJSON(200, signedVideos);
 }
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,13 +1,14 @@
 import { respondWithJSON } from './json';
 import { updateVideo } from '../db/videos';
 import { type ApiConfig } from '../config';
-import { S3Client, type BunRequest } from 'bun';
+import { file, S3Client, type BunRequest } from 'bun';
 import { BadRequestError, UserForbiddenError } from './errors';
 import { getBearerToken, validateJWT } from '../auth';
 import { getVideo } from '../db/videos';
 import { randomBytes } from 'crypto';
 import { rm } from 'fs/promises';
 import path from 'path';
+import type { Video } from '../db/videos';
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const uploadLimit = 1 << 30;
   const { videoId } = req.params as { videoId?: string };
@@ -22,6 +23,7 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
       'The Userid of the Video is different the the accesing user id'
     );
   }
+
   const formData = await req.formData();
   const videoData = formData.get('video');
   if (!(videoData instanceof File)) {
@@ -34,19 +36,118 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   if (videoType !== 'video/mp4') {
     throw new BadRequestError('The video is not of type mp4');
   }
+
   const randomBytesString = randomBytes(32).toString('base64url');
   const filePath = path.join(cfg.assetsRoot, `${randomBytesString}`);
   await Bun.write(filePath, videoData);
+  let aspectration = await getVideoAspectRatio(filePath);
+
+  const processedOutputPath = await processVideoForFastStart(filePath);
   const s3RandomBytesString = randomBytes(32).toString('hex');
-  const s3key = `${s3RandomBytesString}.mp4`;
-  try {
-    const tempFile = Bun.file(filePath);
-    const s3File = cfg.s3Client.file(s3key);
-    await s3File.write(tempFile, { type: 'video/mp4' });
-    videoMetadata.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${s3key}`;
-  } finally {
-    await rm(filePath, { force: true });
-  }
+  const s3key = `${aspectration}${s3RandomBytesString}.mp4`;
+
+  const tempFile = Bun.file(processedOutputPath);
+  const s3File = cfg.s3Client.file(s3key, { bucket: cfg.s3Bucket });
+  await s3File.write(tempFile, { type: 'video/mp4' });
+  videoMetadata.videoURL = `${s3key}`;
+
+  await rm(filePath, { force: true });
+  await rm(processedOutputPath, { force: true });
+
   updateVideo(cfg.db, videoMetadata);
-  return respondWithJSON(200, videoMetadata);
+  const videoMetadataSigned = await dbVideoToSignedVideo(cfg, videoMetadata);
+  return respondWithJSON(200, videoMetadataSigned);
+}
+
+export async function getVideoAspectRatio(filePath: string) {
+  const proc = Bun.spawn(
+    [
+      'ffprobe',
+      '-v',
+      'error',
+      '-select_streams',
+      'v:0',
+      '-show_entries',
+      'stream=width,height',
+      '-of',
+      'json',
+      filePath,
+    ],
+    { stderr: 'pipe', stdout: 'pipe' }
+  );
+
+  if (!proc) {
+    throw new BadRequestError('proc coult not be gathered');
+  }
+  if (!proc.stderr) {
+    throw new BadRequestError('proc is undifined in stderr');
+  }
+  if (!proc.stdout) {
+    throw new BadRequestError('proc is undifined in stdout');
+  }
+
+  const outOutput = await new Response(proc.stdout).text();
+  const errOutput = await new Response(proc.stderr).text();
+  const exited = await proc.exited;
+  if (exited !== 0) {
+    throw new BadRequestError('the proc exited with error code uneaqual to 0');
+  }
+  let meta = JSON.parse(outOutput);
+  const width = meta.streams[0].width;
+  const height = meta.streams[0].height;
+  const ratio = width / height;
+  if (Math.abs(ratio - 16 / 9) < 0.1) {
+    return 'landscape/';
+  } else if (Math.abs(ratio - 9 / 16) < 0.1) {
+    return 'portrait/';
+  } else {
+    return 'other/';
+  }
+}
+
+export async function processVideoForFastStart(inputFilePath: string) {
+  let outputPath = inputFilePath + '.processed';
+  let proc = Bun.spawn(
+    [
+      'ffmpeg',
+      '-i',
+      inputFilePath,
+      '-movflags',
+      'faststart',
+      '-map_metadata',
+      '0',
+      '-codec',
+      'copy',
+      '-f',
+      'mp4',
+      outputPath,
+    ],
+    { stderr: 'pipe', stdout: 'pipe' }
+  );
+  const exited = await proc.exited;
+  if (exited !== 0) {
+    throw new BadRequestError('the proc exited with error code uneaqual to 0');
+  }
+  return outputPath;
+}
+
+export async function generatePresignedURL(
+  cfg: ApiConfig,
+  key: string,
+  expireTime: number
+) {
+  const signedURL = cfg.s3Client.presign(`${key}`, {
+    expiresIn: expireTime,
+  });
+  return signedURL;
+}
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
+  const key = video.videoURL;
+  if (!key) {
+    return video;
+  }
+  const preSignedUrl = await generatePresignedURL(cfg, key, 360);
+  video.videoURL = preSignedUrl;
+
+  return video;
 }
